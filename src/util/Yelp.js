50f16b2a135ca2ae4ab03ba496747ab20cf27e6b
anandgarikapati <garikapati.2014@gmail.com>
2022-01-28 19:53:56 +0530

    added cors

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,8 @@ const Yelp = {
     searchYelp(term, location, sortBy) {
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
             headers: {
-                Authorization: `Bearer ${apiKey}`
+                Authorization: `Bearer ${apiKey}`,
+                'Access-Control-Allow-Origin':'*'
             }
         })
         .then((response) => {
